24c8575fef5ae4361c1aad4f6825004c4b941489
Niccolò Brembilla <niccolo1.brembilla@mail.polimi.it>
2023-03-02 15:23:12 +0100

    Initial commit

---
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
