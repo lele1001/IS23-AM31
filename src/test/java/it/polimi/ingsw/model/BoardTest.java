e154ee5cc018565c0a2e67e121bbe335d71a27aa
Niccolò Brembilla <niccolo1.brembilla@mail.polimi.it>
2023-03-03 16:08:41 +0100

    Added basic classes for model and the related Tests

---
+++ src/test/java/it/polimi/ingsw/model/BoardTest.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BoardTest {
+
+    @Test
+    public void Board() {
+        assertTrue(true);
+    }
+}
