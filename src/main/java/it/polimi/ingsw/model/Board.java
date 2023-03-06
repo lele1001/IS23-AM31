f1299a98bd72f8474be5c98c3e317a3174ff28c1
Niccolò Brembilla <niccolo1.brembilla@mail.polimi.it>
2023-03-06 10:47:35 +0100

    All board methods tested

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -6,9 +6,9 @@ import java.util.Collections;
 
 public class Board {
     Cell [][] board = new Cell [9][9];
-    private ArrayList<ItemCard> cardBag = new ArrayList<>();
+    ArrayList<ItemCard> cardBag = new ArrayList<>();
 
-    //Arraylist used for creating the board using the minum number of player to put an Itemcard in the Cell
+    //Arraylist used for creating the board using the minimum number of player to put an Itemcard in the Cell
     private final ArrayList<Integer> numplayers2 = new ArrayList<>((Arrays.asList(13, 14, 23, 24, 25, 32, 33, 34, 35, 36, 37, 41, 42, 43, 44, 45, 46, 47, 51, 52, 53, 54, 55, 56, 63, 64, 65, 74, 75)));
     private final ArrayList<Integer> Numplayers3=new ArrayList<>(Arrays.asList(3,22,26,38,50,62,66,85));
     private final ArrayList<Integer> Numplayers4=new ArrayList<>(Arrays.asList(4,15,31,40,48,57,73,84));
@@ -32,7 +32,7 @@ public class Board {
         for(Cell[] cells : board){
             for(Cell cell : cells) {
                 if (cell.itemCard == null) {
-                    if (cell.numPlayermin < NumofPlayers) {
+                    if (cell.numPlayermin <= NumofPlayers) {
                         cell.itemCard = cardBag.get(0);
                         cardBag.remove(0);
                     }
@@ -47,19 +47,21 @@ public class Board {
         boolean refill=true;
         for(int i=0;i< board.length && refill;i++) {
             for (int j = 0; j < board[0].length && refill; j++) {
-                if((i==0||i==8)||(j==0||j==8) && board[i][j].itemCard!=null) refill=checkSide(i,j);
-                else if(board[i][j].itemCard!=null) refill=checkPosition(i,j);
+                if(((i==0||i==8)||(j==0||j==8)) && board[i][j].itemCard!=null && board[i][j].numPlayermin<=NumofPlayers) refill=checkSide(i,j);
+                else if(board[i][j].itemCard!=null&&board[i][j].numPlayermin<=NumofPlayers) refill=checkPosition(i,j);
             }
         }
-        System.out.println("REFILLING THE BOARD");
-        if(refill) fillBoard(NumofPlayers);
+        if(refill) {
+            System.out.println("REFILLING THE BOARD");
+            fillBoard(NumofPlayers);
+        }
     }
     public boolean checkSide(int i,int j){
         if(i==0){
-            if (board[i+1][j].itemCard!=null||board[i][j+1].itemCard!=null||board[i+1][j-1].itemCard!=null) return false;
+            if (board[i+1][j].itemCard!=null||board[i][j+1].itemCard!=null||board[i][j-1].itemCard!=null) return false;
         }
         if(i==8){
-            if (board[i-1][j].itemCard!=null||board[i][j+1].itemCard!=null||board[i+1][j-1].itemCard!=null) return false;
+            if (board[i-1][j].itemCard!=null||board[i][j+1].itemCard!=null||board[i][j-1].itemCard!=null) return false;
         }
         if(j==0){
             if (board[i+1][j].itemCard!=null||board[i-1][j].itemCard!=null||board[i][j+1].itemCard!=null) return false;
@@ -72,23 +74,35 @@ public class Board {
     public boolean checkPosition(int i,int j){
         return board[i+1][j].itemCard == null && board[i-1][j].itemCard == null && board[i][j-1].itemCard == null && board[i][j+1].itemCard == null;
     }
+
+    // The check if there isn't an Itemcard is not done here
     public boolean checkSelection(ArrayList<Integer> Position){
         Collections.sort(Position);
-        if((Position.get(0) == Position.get(1) -10)&&(Position.get(1)==Position.get(2)-10)) return true;
-        if((Position.get(0) == Position.get(1) - 1) && (Position.get(1) == Position.get(2) - 1)) return true;
+        if(Position.size()==3) {
+            if ((Position.get(0) == Position.get(1) - 10) && (Position.get(1) == Position.get(2) - 10)) return true;
+            if ((Position.get(0) == Position.get(1) - 1) && (Position.get(1) == Position.get(2) - 1)) return true;
+        }
+        if(Position.size()==2) {
+            if (Position.get(0) == Position.get(1) - 10) return true;
+            if (Position.get(0) == Position.get(1) - 1) return true;
+        }
         return false;
     }
     public ArrayList<ItemCard> deleteSelection(ArrayList<Integer> Position){
         Collections.sort(Position);
         ArrayList<ItemCard> chosencard=new ArrayList<>();
         for(int pos : Position){
+            if(board[pos / 10][pos % 10].itemCard==null) System.out.println("Not find card at"+ pos / 10 + " " + pos % 10);
             chosencard.add(board[pos / 10][pos % 10].itemCard);
             board[pos/10][pos%10].itemCard=null;
         }
         return chosencard;
     }
+    //Todo for Mila, already created ItemCard
     public void createcardBag(){
-
+        for(int i=0;i<132;i++){
+            cardBag.add(new ItemCard());
+        }
     }
     public void printBoard(){
         //Todo when ItemCards are created (print must be chosen on what time of card we have in the cell)
--- src/test/java/it/polimi/ingsw/model/BoardTest.java
+++ src/test/java/it/polimi/ingsw/model/BoardTest.java
@@ -2,12 +2,221 @@ package it.polimi.ingsw.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+    @Test
+    public void TestBoardcreation() {
+        Board boardtest=new Board();
+        int [][] player;
+        player= new int[][]{
+                {5, 5, 5, 3, 4, 5, 5, 5, 5},
+                {5, 5, 5, 2, 2, 4, 5, 5, 5},
+                {5, 5, 3, 2, 2, 2, 3, 5, 5},
+                {5, 4, 2, 2, 2, 2, 2, 2, 3},
+                {4, 2, 2, 2, 2, 2, 2, 2, 4},
+                {3, 2, 2, 2, 2, 2, 2, 4, 5},
+                {5, 5, 3, 2, 2, 2, 3, 5, 5},
+                {5, 5, 5, 4, 2, 2, 5, 5, 5},
+                {5, 5, 5, 5, 4, 3, 5, 5, 5}
+        };
+        for(int i=0;i< boardtest.board.length;i++) {
+            for (int j = 0; j < boardtest.board[0].length; j++) {
+                assertEquals(null, boardtest.board[i][j].itemCard);
+            }
+        }
+        for(int i=0;i< boardtest.board.length;i++) {
+            for (int j = 0; j < boardtest.board[0].length; j++) {
+                assertEquals(player[i][j], boardtest.board[i][j].numPlayermin);
+            }
+        }
+        assertNotNull(boardtest.cardBag);
+        assertEquals(132,boardtest.cardBag.size());
+    }
 
     @Test
-    public void Board() {
-        assertTrue(true);
+    void fillBoardfromscratch() {
+        int numplayer;
+        for (numplayer = 2; numplayer < 5; numplayer++) {
+            Board boardtest1=new Board();
+            boardtest1.fillBoard(numplayer);
+            for (int i = 0; i < boardtest1.board.length; i++) {
+                for (int j = 0; j < boardtest1.board[0].length; j++) {
+                    if (boardtest1.board[i][j].numPlayermin <= numplayer)
+                        assertNotNull(boardtest1.board[i][j].itemCard);
+                    else
+                        assertNull(boardtest1.board[i][j].itemCard);
+                }
+            }
+            if (numplayer == 2)
+                assertEquals(103, boardtest1.cardBag.size());
+            if (numplayer == 3)
+                assertEquals(95, boardtest1.cardBag.size());
+            if (numplayer == 4)
+                assertEquals(87, boardtest1.cardBag.size());
+        }
+    }
+    @Test
+    void checksideRefill() {
+        int numplayer=3;
+        ItemCard prima;
+        Board boardtest1=new Board();
+        boardtest1.board[0][3].itemCard=boardtest1.cardBag.get(0);
+        boardtest1.cardBag.remove(0);
+        assertNotNull( boardtest1.board[0][3].itemCard);
+        prima=boardtest1.board[0][3].itemCard;
+        boardtest1.checkRefill(numplayer);
+        assertEquals(prima,boardtest1.board[0][3].itemCard);
+        for (int i = 0; i < boardtest1.board.length; i++) {
+            for (int j = 0; j < boardtest1.board[0].length; j++) {
+                if (boardtest1.board[i][j].numPlayermin <= numplayer)
+                    assertNotNull(boardtest1.board[i][j].itemCard);
+                else
+                    assertNull(boardtest1.board[i][j].itemCard);
+            }
+        }
+    }
+    @Test
+    void checksidenotRefill() {
+        int numplayer=3;
+        Board prima=new Board();
+        Board boardtest1=new Board();
+        boardtest1.board[0][3].itemCard=boardtest1.cardBag.get(0);
+        boardtest1.cardBag.remove(0);
+        boardtest1.board[1][3].itemCard=boardtest1.cardBag.get(0);
+        boardtest1.cardBag.remove(0);
+        assertNotNull( boardtest1.board[0][3].itemCard);
+        assertNotNull( boardtest1.board[1][3].itemCard);
+        for(int i=0;i< boardtest1.board.length;i++) {
+            for (int j = 0; j < boardtest1.board[0].length; j++) {
+                prima.board[i][j].itemCard=boardtest1.board[i][j].itemCard;
+            }
+        }
+        for(int i=0;i< boardtest1.board.length;i++) {
+            for (int j = 0; j < boardtest1.board[0].length; j++) {
+                assertEquals(prima.board[i][j].itemCard,boardtest1.board[i][j].itemCard);
+            }
+        }
+        boardtest1.checkRefill(numplayer);
+        //The board has not changed
+        for(int i=0;i< boardtest1.board.length;i++) {
+            for (int j = 0; j < boardtest1.board[0].length; j++) {
+                assertEquals(prima.board[i][j].itemCard,boardtest1.board[i][j].itemCard);
+            }
+        }
+    }
+
+    @Test
+    void checkcenterRefill() {
+        int numplayer=2;
+        Board prima;
+        Board boardtest1=new Board();
+        boardtest1.board[3][3].itemCard=boardtest1.cardBag.get(0);
+        boardtest1.cardBag.remove(0);
+        assertNotNull( boardtest1.board[3][3].itemCard);
+        prima=boardtest1;
+        assertEquals(boardtest1,prima);
+        boardtest1.checkRefill(numplayer);
+        assertEquals(prima.board[3][3].itemCard,boardtest1.board[3][3].itemCard);
+        for (int i = 0; i < boardtest1.board.length; i++) {
+            for (int j = 0; j < boardtest1.board[0].length; j++) {
+                if (boardtest1.board[i][j].numPlayermin <= numplayer)
+                    assertNotNull(boardtest1.board[i][j].itemCard);
+                else
+                    assertNull(boardtest1.board[i][j].itemCard);
+            }
+        }
+    }
+    @Test
+    void checkcenternotRefill() {
+        int numplayer=2;
+        Board prima;
+        Board boardtest1=new Board();
+        boardtest1.board[3][3].itemCard=boardtest1.cardBag.get(0);
+        boardtest1.cardBag.remove(0);
+        boardtest1.board[3][4].itemCard=boardtest1.cardBag.get(0);
+        boardtest1.cardBag.remove(0);
+        assertNotNull( boardtest1.board[3][3].itemCard);
+        assertNotNull( boardtest1.board[3][4].itemCard);
+        prima=boardtest1;
+        assertEquals(boardtest1,prima);
+        boardtest1.checkRefill(numplayer);
+        //The board has not changed
+        assertEquals(prima.board,boardtest1.board);
+    }
+    @Test
+    void checkSelection2integer() {
+        Board boardtest1=new Board();
+        boolean res;
+        ArrayList<Integer> pos= new ArrayList<>(List.of(20,30));
+        res=boardtest1.checkSelection(pos);
+        assertEquals(true,res);
+        pos= new ArrayList<>(List.of(21,20));
+        res=boardtest1.checkSelection(pos);
+        assertEquals(true,res);
+        pos= new ArrayList<>(List.of(31,20));
+        res=boardtest1.checkSelection(pos);
+        assertEquals(false,res);
+    }
+    @Test
+    void checkSelection3integer() {
+        Board boardtest1=new Board();
+        boolean res;
+        ArrayList<Integer> pos= new ArrayList<>(List.of(20,30,40));
+        res=boardtest1.checkSelection(pos);
+        assertEquals(true,res);
+        pos= new ArrayList<>(List.of(21,20,19));
+        res=boardtest1.checkSelection(pos);
+        assertEquals(true,res);
+        pos= new ArrayList<>(List.of(31,20,21));
+        res=boardtest1.checkSelection(pos);
+        assertEquals(false,res);
+    }
+
+    @Test
+    void deleteSelection() {
+        Board boardtest1=new Board();
+        Board prima=new Board();
+        int position;
+        int numplayer=2;
+        ArrayList<Integer> pos= new ArrayList<>(List.of(34,33));
+        ArrayList<ItemCard> delete;
+        boardtest1.fillBoard(numplayer);
+        for(int i=0;i< boardtest1.board.length;i++) {
+            for (int j = 0; j < boardtest1.board[0].length; j++) {
+                prima.board[i][j].itemCard=boardtest1.board[i][j].itemCard;
+            }
+        }
+        for(int i=0;i< boardtest1.board.length;i++) {
+            for (int j = 0; j < boardtest1.board[0].length; j++) {
+                assertEquals(prima.board[i][j].itemCard,boardtest1.board[i][j].itemCard);
+            }
+        }
+        delete=boardtest1.deleteSelection(pos);
+        assertEquals(2,delete.size());
+        for (int i = 0; i < boardtest1.board.length; i++) {
+            for (int j = 0; j < boardtest1.board[0].length; j++) {
+                position=10*i+j;
+                if (!pos.contains(position)){
+                    assertEquals(prima.board[i][j].itemCard,boardtest1.board[i][j].itemCard);
+                }
+                else
+                {
+                    assertTrue(delete.contains(prima.board[i][j].itemCard));
+                }
+            }
+        }
+
+    }
+
+    @Test
+    void createcardBag() {
+    }
+
+    @Test
+    void printBoard() {
     }
 }
