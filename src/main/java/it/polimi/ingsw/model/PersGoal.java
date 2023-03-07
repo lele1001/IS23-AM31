9f664003cee4d288fb67712bd502ffafc5a92a59
luigibruzzese <luigi.bruzzese@mail.polimi.it>
2023-03-07 09:22:31 +0100

    First implementation of Library and LibraryTest

--- src/main/java/it/polimi/ingsw/model/PersGoal.java
+++ src/main/java/it/polimi/ingsw/model/PersGoal.java
@@ -6,21 +6,21 @@ import java.util.*;
 import static it.polimi.ingsw.model.HouseItem.*;
 
 public enum PersGoal {
-    Card1(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card2(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card3(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card4(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card5(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card6(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card7(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card8(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card9(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card10(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card11(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat))),
-    Card12(new ArrayList<>(List.of(10,20,21,23,34,35)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Cat)));
+    Card1(new ArrayList<>(List.of(2,14,23,31,52,0)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card2(new ArrayList<>(List.of(54,20,34,22,43,11)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card3(new ArrayList<>(List.of(10,20,50,31,34,22)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card4(new ArrayList<>(List.of(32,52,51,4,30,43)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card5(new ArrayList<>(List.of(31,53,32,50,11,44)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card6(new ArrayList<>(List.of(43,4,23,41,2,50)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card7(new ArrayList<>(List.of(13,0,52,44,30,21)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card8(new ArrayList<>(List.of(4,11,43,53,22,30)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card9(new ArrayList<>(List.of(50,22,34,2,41,44)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card10(new ArrayList<>(List.of(41,33,20,11,4,53)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card11(new ArrayList<>(List.of(32,44,11,20,53,2)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants))),
+    Card12(new ArrayList<>(List.of(22,50,2,44,33,11)), new ArrayList<>(List.of(Frame, Cat, Books, Games, Trophy, Plants)));
 
     // Lista ordinata dei punteggi in base al numero (l'indice della lista) di tessere posizionate correttamente
-    private final static List<Integer> scoreList = new ArrayList<>(List.of(1,2,4,6,9,12));
+    private final static List<Integer> scoreList = new ArrayList<>(List.of(0,1,2,4,6,9,12));
 
     // Ad ogni posizione (l'Integer) associa l'HouseItem che ci deve andare
     private final Map<Integer, HouseItem> positions = new HashMap<>(6);
--- src/test/java/it/polimi/ingsw/model/LibraryTest.java
+++ src/test/java/it/polimi/ingsw/model/LibraryTest.java
@@ -5,31 +5,83 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static it.polimi.ingsw.model.HouseItem.Cat;
-import static it.polimi.ingsw.model.HouseItem.Frame;
-import static it.polimi.ingsw.model.ItemNumber.First;
-import static it.polimi.ingsw.model.ItemNumber.Second;
+import static it.polimi.ingsw.model.HouseItem.*;
+import static it.polimi.ingsw.model.ItemNumber.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
 
     @Test
     void insertCard() {
-        Library l = new Library();
+        Library l = new Library(); int i;
         l.insertCard(new ArrayList<>(List.of(new ItemCard(Cat, First), new ItemCard(Frame, Second))), 2);
         assertEquals(l.get(5,2), new ItemCard(Cat, First));
+        assertEquals(l.get(4, 2), new ItemCard(Frame, Second));
+
+        l.insertCard(new ArrayList<>(List.of(new ItemCard(Books, Third))), 2);
+        assertEquals(l.get(5,2), new ItemCard(Cat, First));
+        assertEquals(l.get(4, 2), new ItemCard(Frame, Second));
+        assertEquals(l.get(3, 2), new ItemCard(Books, Third));
+
+        l.insertCard(new ArrayList<>(List.of(new ItemCard(Books, Third))), 1);
+        assertEquals(l.get(5, 1), new ItemCard(Books, Third));
+        for (i = 4; i>=0; i--)
+            assertNull(l.get(i, 1));
+
+        l.insertCard(new ArrayList<>(List.of(new ItemCard(Cat, Second), new ItemCard(Books, First))), 4);
+        l.insertCard(new ArrayList<>(List.of(new ItemCard(Frame, Second), new ItemCard(Books, Third))), 4);
+        l.insertCard(new ArrayList<>(List.of(new ItemCard(Cat, First), new ItemCard(Plants, First))), 4);
+        assertEquals(l.get(5, 4), new ItemCard(Cat, Second));
+        assertEquals(l.get(4, 4), new ItemCard(Books, First));
+        assertEquals(l.get(3, 4), new ItemCard(Frame, Second));
+        assertEquals(l.get(2, 4), new ItemCard(Books, Third));
+        assertEquals(l.get(1, 4), new ItemCard(Cat, First));
+        assertEquals(l.get(0, 4), new ItemCard(Plants, First));
+
     }
 
-    @Test
-    void checkSpace() {
-        Library l = new Library();
 
+    // Full library, always returns false
+    @Test
+    void checkSpace1() {
+        Library l = new Library();
+        l.insertCard(new ArrayList<>(List.of(new ItemCard(Cat, First), new ItemCard(Frame, Second), new ItemCard(Books, Third), new ItemCard(Cat, First), new ItemCard(Frame, Second), new ItemCard(Books, Third))), 0);
+        l.insertCard(new ArrayList<>(List.of(new ItemCard(Cat, First), new ItemCard(Frame, Second), new ItemCard(Books, Third), new ItemCard(Cat, First), new ItemCard(Frame, Second), new ItemCard(Books, Third))), 1);
+        l.insertCard(new ArrayList<>(List.of(new ItemCard(Cat, First), new ItemCard(Frame, Second), new ItemCard(Books, Third), new ItemCard(Cat, First), new ItemCard(Frame, Second), new ItemCard(Books, Third))), 2);
+        l.insertCard(new ArrayList<>(List.of(new ItemCard(Cat, First), new ItemCard(Frame, Second), new ItemCard(Books, Third), new ItemCard(Cat, First), new ItemCard(Frame, Second), new ItemCard(Books, Third))), 3);
+        l.insertCard(new ArrayList<>(List.of(new ItemCard(Cat, First), new ItemCard(Frame, Second), new ItemCard(Books, Third), new ItemCard(Cat, First), new ItemCard(Frame, Second), new ItemCard(Books, Third))), 4);
+        assertEquals(l.checkSpace(0, 1), false);
+        assertEquals(l.checkSpace(0, 3), false);
+        assertEquals(l.checkSpace(1, 1), false);
+        assertEquals(l.checkSpace(1, 3), false);
+        assertEquals(l.checkSpace(2, 1), false);
+        assertEquals(l.checkSpace(2, 2), false);
+        assertEquals(l.checkSpace(3, 1), false);
+        assertEquals(l.checkSpace(3, 6), false);
+        assertEquals(l.checkSpace(4, 1), false);
+        assertEquals(l.checkSpace(4, 3), false);
+        assertEquals(l.checkSpace(5, 1), false);
+        assertEquals(l.checkSpace(5, 2), false);
+    }
+
+    // Empty library, always returns true
+    @Test
+    void checkSpace2() {
+        Library l = new Library();
+        assertEquals(l.checkSpace(0, 1), true);
+        assertEquals(l.checkSpace(0, 3), true);
+        assertEquals(l.checkSpace(1, 1), true);
+        assertEquals(l.checkSpace(1, 2), true);
+        assertEquals(l.checkSpace(2, 1), true);
+        assertEquals(l.checkSpace(2, 3), true);
+        assertEquals(l.checkSpace(3, 2), true);
+        assertEquals(l.checkSpace(3, 3), true);
+        assertEquals(l.checkSpace(4, 1), true);
+        assertEquals(l.checkSpace(4, 2), true);
     }
 
     @Test
     void get() {
-        Library l = new Library();
-
     }
 
 
