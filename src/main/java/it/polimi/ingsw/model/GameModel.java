50e03de9cae98013ec3d20f0ab2e9eea702fd612
MilagrosCasaperalta <milagrosadriana.casaperalta@mail.polimi.it>
2023-03-21 16:33:48 +0100

    Added all classe in gamemodel

--- src/main/java/it/polimi/ingsw/model/GameModel.java
+++ src/main/java/it/polimi/ingsw/model/GameModel.java
@@ -1,12 +1,12 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.ModelExceptions.EmptyCardBagException;
 import it.polimi.ingsw.ModelExceptions.NoBookshelfSpaceException;
 import it.polimi.ingsw.ModelExceptions.NoRightItemCardSelection;
+import it.polimi.ingsw.model.ComGoals.*;
 
 import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class GameModel {
 
@@ -15,15 +15,32 @@ public class GameModel {
 
     PropertyChangeListener listener;
 
+    private final ArrayList<ComGoal> comGoals = new ArrayList<>();
     /**
      * Builds the game
      * @param players list of nicknames
      */
     public GameModel(ArrayList<String> players) {
+
+        board = new Board(players.size());
+        Random random = new Random();
+        int firstGoal= 0;
+        int secondGoal = 0;
+        do{
+            firstGoal= random.nextInt(1, 12);
+            secondGoal = random.nextInt(1, 12);
+        } while(firstGoal != secondGoal);
+
+        comGoals.add(selectComGoal(firstGoal, players.size()));
+        comGoals.add(selectComGoal(secondGoal, players.size()));
+
+        ArrayList<PersGoal> persGoals = new ArrayList<>(Arrays.asList(PersGoal.values()));
+        Collections.shuffle(persGoals);
         for(String s: players) {
             this.playerMap.put(s, new Player(s));
+            playerMap.get(s).assignPersGoal(persGoals.get(0));
+            persGoals.remove(0);
         }
-        board = new Board(players.size());
     }
 
     /**
@@ -33,7 +50,7 @@ public class GameModel {
      * @param column where cards have to be insert
      * @throws NoBookshelfSpaceException if there's no space
      */
-    void InsertCard(String nickname, ArrayList<ItemCard> cards, int column) throws NoBookshelfSpaceException { //l'eccezione verrà gestita nel controller
+    public void InsertCard(String nickname, ArrayList<ItemCard> cards, int column) throws NoBookshelfSpaceException { //l'eccezione verrà gestita nel controller
             playerMap.get(nickname).insertCard(cards, column);
             //PropertyChangeEvent evt = new PropertyChangeEvent(//da mettere);
             //this.listener.properyChange(evt);
@@ -45,7 +62,7 @@ public class GameModel {
      * @param positions cards' positions
      * @throws NoRightItemCardSelection if not all of the positions are available
      */
-    void selectCard(ArrayList<Integer> positions) throws NoRightItemCardSelection {  //anche questa eccezione verrà gestita nel controller
+    public void selectCard(ArrayList<Integer> positions) throws NoRightItemCardSelection {  //anche questa eccezione verrà gestita nel controller
 
         ArrayList<ItemCard> deleted;
            deleted = board.deleteSelection(positions);
@@ -57,7 +74,7 @@ public class GameModel {
      * set the controller as listener
      * @param listener
      */
-    void setListener(PropertyChangeListener listener){
+    public void setListener(PropertyChangeListener listener){
         this.listener = listener;
     }
 
@@ -66,7 +83,7 @@ public class GameModel {
      * @param nickname nickname of the player to be checked
      * @return true if the player is online
      */
-    boolean isPlayerOnline(String nickname){
+    public boolean isPlayerOnline(String nickname){
        return playerMap.get(nickname).isOnline();
     }
 
@@ -74,7 +91,7 @@ public class GameModel {
      * calculates all the players' final score and sends it to each of them
      * @return returns the nickname of the winner
      */
-    String calcFinalScore() { //su tutit i player sulla mappa devo chiamare il metodo per calcolare il punteggio
+    public String calcFinalScore() { //su tutit i player sulla mappa devo chiamare il metodo per calcolare il punteggio
         //salvo punteggio e confronto quello più alto
         //return nickname di quello col punteggio più alto
         int temp;
@@ -91,11 +108,51 @@ public class GameModel {
         return nome;
     }
 
-    void changePlayerStatus(String nickname) {
+    public void changePlayerStatus(String nickname) {
         playerMap.get(nickname).changePlayerStatus();
 
     }
 
+    private ComGoal selectComGoal(int numComGoal, int numPlayers){
+
+        switch (numComGoal){
+            case 1:
+                return new CG1(numPlayers);
+            case 2:
+                return new CG2_5(numPlayers, 6);
+            case 3:
+                return new CG3_4(numPlayers, 2);
+            case 4:
+                return new CG3_4(numPlayers, 1);
+            case 5:
+                return new CG2_5(numPlayers, 3);
+            case 6:
+                return new CG6_7(numPlayers, 5);
+            case 7:
+                return new CG6_7(numPlayers, 3);
+            case 8:
+                return new CG8(numPlayers);
+            case 9:
+                return new CG9(numPlayers);
+            case 10:
+                return new CG10(numPlayers);
+            case 11:
+                return new CG11_12(numPlayers, true);
+            case 12:
+                return new CG11_12(numPlayers, false);
+            default:
+                return null;
+        }
+    }
+
+    public void EndTurn(String nickname) throws EmptyCardBagException {
+        boolean ComGoalDone;
+        for(ComGoal c: comGoals) {
+            ComGoalDone=playerMap.get(nickname).checkComGoal(c);
+            if(ComGoalDone); //if true listener update
+        }
+        board.checkRefill();
+    }
 }
 
 
---
+++ src/test/java/it/polimi/ingsw/model/ComGoals/ComGoalTest.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.model.ComGoals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ComGoalTest {
+
+    @Test
+    void getCurrScore() {
+        ComGoal comGoal = new CG1(2);
+        assertEquals(8,comGoal.getCurrScore());
+        comGoal.score.remove(0);
+        assertEquals(4,comGoal.getCurrScore());
+        comGoal.score.remove(0);
+        assertEquals(0,comGoal.getCurrScore());
+    }
+}
