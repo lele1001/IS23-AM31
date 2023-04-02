9c3dadfb2133886533d520e172273c2dbbe02dd0
MilagrosCasaperalta <milagrosadriana.casaperalta@mail.polimi.it>
2023-04-02 11:59:00 +0200

    Creation of the for GameController

--- src/test/java/it/polimi/ingsw/server/controller/GameControllerTest.java
+++ src/test/java/it/polimi/ingsw/server/controller/GameControllerTest.java
@@ -2,18 +2,43 @@ package it.polimi.ingsw.server.controller;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GameControllerTest {
 
     @Test
     void createGame() {
-        GameController gameController = new GameController(new ConnectionControl());
-        gameController.addPlayer("Andrea");
-        gameController.addPlayer("Luca");
-        gameController.addPlayer("Alice");
-        gameController.createGame();
-        //rifare stessa roba con più giocatori o meno
-        //controllo che non inserisca più di 4 giocatori -> provo ad aggiungere più giocatori >4 e controllo che la stringa del metodo add player esca
+
+        System.out.println("Case: 2 players");
+
+        GameController gameController2 = new GameController(new ConnectionControl());
+        gameController2.addPlayer("Giacomo");
+        gameController2.addPlayer("Nicolas");
+        gameController2.createGame();
+
+        System.out.println("Case: 3 players");
+
+        GameController gameController3 = new GameController(new ConnectionControl());
+        gameController3.addPlayer("Andrea");
+        gameController3.addPlayer("Luca");
+        gameController3.addPlayer("Alice");
+        gameController3.createGame();
+
+
+        System.out.println("Case: 4 players");
+
+        GameController gameController4 = new GameController(new ConnectionControl());
+        gameController4.addPlayer("Topolino");
+        gameController4.addPlayer("Eleonora");
+        gameController4.addPlayer("Luigi");
+        gameController4.addPlayer("Niccolò");
+        gameController4.createGame();
+
+        System.out.println("Case: 5 players");
+
+        GameController gameController5 = new GameController(new ConnectionControl());
+        gameController5.addPlayer("Riccardo");
+        gameController5.addPlayer("Eleonora");
+        gameController5.addPlayer("Alice");
+        gameController5.addPlayer("Topolino");
+        gameController5.addPlayer("Andrea");
     }
 }
